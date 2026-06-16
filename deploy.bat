46eafce1269383f6d87c36b79bb6e11e61daa7ec
jaxon-hash <jaxongarforth@gmail.com>
2026-06-16 22:18:14 +0100

    Auto-update portfolio via deploy script

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+echo 🚀 Starting Deployment to jxnn.store...
+git add .
+git commit -m "Auto-update portfolio via deploy script"
+echo.
+echo ✅ Changes have been pushed to GitHub!
+echo.
+pause
