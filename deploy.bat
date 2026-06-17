fd1df07ead7568b77ee420a81caa3b3034456872
jaxon-hash <jaxongarforth@gmail.com>
2026-06-17 10:29:19 +0100

    Update portfolio: 17/06/2026 10:29:19.22

--- deploy.bat
+++ deploy.bat
@@ -2,8 +2,11 @@
 echo 🚀 Starting Deployment to jxnn.store...
 
 git add .
-git commit -m "Update portfolio: %date% %time%"
+:: The '|| ver > nul' allows the script to continue even if there are no new changes to commit
+git commit -m "Update portfolio: %date% %time%" || ver > nul
 echo.
+echo 📥 Syncing changes from other devices...
+git pull origin main --rebase
 echo 📤 Uploading to GitHub...
 git push origin main
 echo ✅ Changes have been pushed to GitHub!
